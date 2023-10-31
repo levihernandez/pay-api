42f7731288c2d653eecd117850270a0d78455d89
Julian Levi Hernandez <35246411+levihernandez@users.noreply.github.com>
2023-10-31 09:24:04 -0400

    Add files via upload

---
+++ pgx-api/pgx-checkBalances.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
+
+export default function () {
+  const uuid = 'a98216bb-9b3d-4d57-8da3-dda89ac03b51';  // Replace with a valid user UUID
+  const response = http.get(`http://localhost:8087/users/${uuid}/balances`);
+  check(response, { 'status was 200': (r) => r.status === 200 });
+}
